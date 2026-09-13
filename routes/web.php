23dd0e09da462eba5c8fbb1c628b6c3876550b41
Kazuki Hisamitsu <hisamitsu0619.0619@gmail.com>
2026-09-13 20:11:31 +0900

    feat：問い合わせCRUD実装  #4

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => 'お問い合わせフォーム（準備中）');
+// お問い合わせフォームのルーティング（認証なし）
+Route::get('/', [ContactController::class, 'index'])->name('contact.index');
+Route::post('/contacts/confirm', [ContactController::class, 'confirm'])->name('contacts.confirm');
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+Route::get('/thanks', [ContactController::class, 'thanks'])->name('contacts.thanks');
 
+// 管理用画面のルーティング（要認証）
 Route::middleware('auth')->group(function () {
     Route::get('/admin', fn () => 'お問い合わせ一覧（準備中）')->name('admin.index');
 });
