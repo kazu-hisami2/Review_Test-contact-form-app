42c3f530eeb6f6b6eb40a05ddb51f325c5302744
Kazuki Hisamitsu <hisamitsu0619.0619@gmail.com>
2026-09-14 02:37:43 +0900

    feat：管理者用のCRUD実装 #5

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreContactRequest;
 use App\Models\Category;
 use App\Models\Contact;
-use App\Models\Tag; // 配列操作用(@store)
+use App\Models\Tag;
 use Illuminate\Support\Arr;
 
 class ContactController extends Controller
@@ -21,9 +21,9 @@ class ContactController extends Controller
     public function confirm(StoreContactRequest $request)
     {
         $validated = $request->validated();
-        $request->flash(); // "store"されるまで入力値を保持
+        $request->flash();
         $category = Category::find($validated['category_id']);
-        $tags = Tag::findMany($validated['tag_ids'] ?? []); // $tag_idsが設定されていなければnull
+        $tags = Tag::findMany($validated['tag_ids'] ?? []);
 
         return view('contact.confirm', compact(['validated', 'category', 'tags']));
     }
@@ -31,9 +31,8 @@ class ContactController extends Controller
     public function store(StoreContactRequest $request)
     {
         $validated = $request->validated();
-        $contact = Contact::create(Arr::except($validated, ['tag_ids'])); // $tag_ids以外の項目を登録
+        $contact = Contact::create(Arr::except($validated, ['tag_ids']));
 
-        // $tag_idsが空じゃないなら、中間テーブルに外部キーの挿入
         if (! empty($validated['tag_ids'])) {
             $contact->tags()->sync($validated['tag_ids']);
         }
--- app/Http/Requests/StoreContactRequest.php
+++ app/Http/Requests/StoreContactRequest.php
@@ -23,17 +23,17 @@ class StoreContactRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'gender' => 'required|integer|in:1,2,3',
-            'email' => 'required|string|email|max:255',
-            'tel' => 'required|string|regex:/^[0-9]{10,11}$/',
-            'address' => 'required|string|max:255',
-            'building' => 'nullable|string|max:255',
-            'category_id' => 'required|integer|exists:categories,id',
-            'detail' => 'required|string|max:120',
-            'tag_ids' => 'nullable|array',
-            'tag_ids.*' => 'integer|exists:tags,id',
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'gender' => ['required', 'integer', 'in:1,2,3'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'tel' => ['required', 'string', 'regex:/^[0-9]{10,11}$/'],
+            'address' => ['required', 'string', 'max:255'],
+            'building' => ['nullable', 'string', 'max:255'],
+            'category_id' => ['required', 'integer', 'exists:categories,id'],
+            'detail' => ['required', 'string', 'max:120'],
+            'tag_ids' => ['nullable', 'array'],
+            'tag_ids.*' => ['integer', 'exists:tags,id'],
         ];
     }
 
